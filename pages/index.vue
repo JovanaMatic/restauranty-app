c7956bf74c4d4b3348efde110f764eca099b23d8
Jovana Matic <jovana.stankovic85@yahoo.com>
2024-02-13 12:11:12 +0100

    Create dynamic page and handle undefined routes

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <Nav />
+    <div class="container">
+      <h1>Welcome to Restaurantly</h1>
+      <a href="/restaurants">Go to restaurants</a>
+    </div>
+  </div>
+</template>
+
+<style>
+
+.container {
+  text-align: center;
+  margin-top: 5rem;
+}
+</style>
---
+++ pages/restaurants/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <Nav />
+    <div class="container">
+      <RestaurantTable />
+    </div>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
